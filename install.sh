81ffde9a11a485df7f9db7cd2d8eb9f67e8228f7
peddy76 <patrick.brauner@gmail.com>
2013-06-30 12:25:30 +0200

    New Plugins and Settings in .vimrc

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
 
-sudo apt-get install vim tmux mutt cmus cifs-utils samba weechat git rdesktop synergy
+sudo apt-get install vim-nox tmux mutt cmus cifs-utils samba weechat git rdesktop synergy \
+    nmap
 
+cb-dropbox-pipemenu --install-dropbox
