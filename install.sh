e13692f736d1dce075a75be733a3d73ab3cfa288
peddy76 <patrick.brauner@gmail.com>
2013-07-03 06:51:17 +0200

    Added a new Plugin

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 sudo apt-get install vim-nox tmux mutt cmus cifs-utils samba weechat git rdesktop synergy \
-    nmap
+    nmap w3m-img
 
 cb-dropbox-pipemenu --install-dropbox
