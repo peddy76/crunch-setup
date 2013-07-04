1285c055e12e21029a41cff97ff0d5236108b409
peddy76 <patrick.brauner@gmail.com>
2013-07-04 14:01:29 +0200

    New Plugins

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 sudo apt-get install vim-nox tmux mutt cmus cifs-utils samba weechat git rdesktop synergy \
-    nmap w3m-img
+    nmap w3m-img cherrytree
 
 cb-dropbox-pipemenu --install-dropbox
