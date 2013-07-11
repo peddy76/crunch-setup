01e6b5a6234034d3c2ad92febf45adc7e3c278d2
peddy76 <patrick.brauner@gmail.com>
2013-07-11 07:06:45 +0200

    Updates

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 sudo apt-get install vim-nox tmux mutt cmus cifs-utils samba weechat git rdesktop synergy \
-    nmap w3m-img cherrytree
+    nmap w3m-img cherrytree exuberant-ctags newsbeuter
 
 cb-dropbox-pipemenu --install-dropbox
